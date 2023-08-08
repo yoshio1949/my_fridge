3ed8fc73c7daff7d69a445af8d5dd467331b59e7
{yoshio1949} <{bump.satomi2331@gmail.com}>
2023-08-08 16:26:03 +0900

    foodでのCRUD機能を実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,12 @@
 class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+  end
+
+  def show
+    @food = Food.find(params[:id])
+  end
+
   def new
     @food = Food.new
   end
@@ -7,6 +15,22 @@ class FoodsController < ApplicationController
     food = Food.new(food_params)
     food.save!
     redirect_to foods_url, notice: "新しい食品「#{food.name}」を登録しました"
+  end
+
+  def edit
+    @food = Food.find(params[:id])
+  end
+
+  def update
+    food = Food.find(params[:id])
+    food.update!(food_params)
+    redirect_to foods_url, notice: "食品「#{food.name}」を更新しました。"
+  end
+
+  def destroy
+    food = Food.find(params[:id])
+    food.destroy
+    redirect_to foods_url, notice: "食品「#{food.name}」を削除しました。"
   end
 
   private

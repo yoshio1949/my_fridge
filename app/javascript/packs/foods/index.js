df4c362270d7423bc2fffb8386976f45f2251f29
{yoshio1949} <{bump.satomi2331@gmail.com}>
2023-08-18 18:36:13 +0900

    [update]app-pageでカラムから詳細ページに飛べるよう実装

---
+++ app/javascript/packs/foods/index.js
@@ -0,0 +1,12 @@
+// jQuery(function ($) {
+jQuery($(document).on('turbolinks:load', function() {
+  $("tbody tr[data-href]").addClass("clickable").click(function () {
+    window.location = $(this).attr("data-href");
+  }).find("a").hover(function () {
+    $(this).parents("tr").unbind("click");
+  },function () {
+    $(this).parents("tr").click(function () {
+      window.location = $(this).attr("data-href");
+    });
+  });
+}));
